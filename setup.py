f706f160a7db6b0aa395d309cf9dadc63685432f
Abhinav-7187 <abhinav.singh1469@gmail.com>
2024-08-15 23:18:15 +0530

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
+def get_requirements(file_path:str)->List[str]: #This function will return a list
     '''
     this function will return the list of requirements
     '''
